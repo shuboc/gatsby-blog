a05544b303d97091867dc7c3daff6a19ba99d43d
Shubo Chao <shubochao@gmail.com>
2022-03-03 20:31:07 +0800

    Try to fix fb comments

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,8 @@
 const React = require("react")
 
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
     <div key="1" id="fb-root"></div>,
-    <script key="2" async defer crossorigin="anonymous" src="https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v13.0&appId=1994883547264823&autoLogAppEvents=1" nonce="VMAKMs28"></script>
+    <script key="2" async defer crossOrigin="anonymous" src="https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v13.0&appId=1994883547264823&autoLogAppEvents=1" nonce="VMAKMs28"></script>
   ])
 }
