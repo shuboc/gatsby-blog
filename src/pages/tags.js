3a174567869e5e461d555728894f243e393fce1f
Shubo Chao <shubochao@gmail.com>
2022-03-05 11:44:59 +0800

    Add tags pages

---
+++ src/pages/tags.js
@@ -0,0 +1,76 @@
+import React from "react"
+import PropTypes from "prop-types"
+import kebabCase from "lodash/kebabCase"
+import { Helmet } from "react-helmet"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+  location
+}) => {
+  const tags = group.map(tag => tag.fieldValue).join(', ')
+  return (
+    <Layout location={location} title={title}>
+      <Seo
+        title="標籤列表"
+        description={tags}
+      />
+      <div>
+        <h1>標籤列表</h1>
+        <ul>
+          {group.map(tag => (
+            <li key={tag.fieldValue}>
+              <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+                {tag.fieldValue} ({tag.totalCount})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
---
+++ src/templates/tags.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Tags = ({ pageContext, data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMarkdownRemark
+  const tagHeader = `${totalCount} 篇有 "${tag}" 標籤的文章`
+  const description = edges.map(({ node }) => node.frontmatter.title).join(', ')
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo
+        title={tagHeader}
+        description={description}
+      />
+      <h1>{tagHeader}</h1>
+      <ul>
+        {edges.map(({ node }) => {
+          const { slug } = node.fields
+          const { title } = node.frontmatter
+          return (
+            <li key={slug}>
+              <Link to={slug}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/tags">所有標籤</Link>
+    </Layout>
+  )
+}
+
+export default Tags
+
+export const pageQuery = graphql`
+  query($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
