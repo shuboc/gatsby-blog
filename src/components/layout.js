1fc7216ab9c9cff27777eb8c33cdcbe13f4c7c7a
Shubo Chao <shubochao@gmail.com>
2026-03-28 11:16:18 +0800

    feat: add full-width sage nav bar, replace conditional header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+    <div data-is-root-path={isRootPath}>
+      <nav className="site-nav">
+        <Link to="/" className="site-nav__logo">SHUBO</Link>
+        <div className="site-nav__links">
+          {isRootPath ? (
+            <Link to="/about" className="site-nav__link">About</Link>
+          ) : (
+            <>
+              <Link to="/" className="site-nav__link">&#8592; All posts</Link>
+              <Link to="/about" className="site-nav__link">About</Link>
+            </>
+          )}
+        </div>
+      </nav>
+      <main className="global-wrapper">
+        {children}
+      </main>
+      <footer className="site-footer">
+        &#169; {new Date().getFullYear()} Shubo
       </footer>
     </div>
   )
