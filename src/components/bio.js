599aaea08233047142b0c0a949e915d04272d11b
Shubo Chao <shubochao@gmail.com>
2026-03-28 11:38:58 +0800

    feat: restyle bio with CSS circle avatar and sage divider row

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,13 +1,5 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -18,39 +10,25 @@ const Bio = () => {
             name
             summary
           }
-          social {
-            twitter
-          }
         }
       }
     }
   `)
 
-  // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
-  // const social = data.site.siteMetadata?.social
+  if (!author?.name) return null
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.jpg"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      {author?.name && (
-        <p>
-          <strong>{author.name}</strong> {author?.summary || null}
-          {/* {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a> */}
-        </p>
-      )}
+      <div className="bio-avatar" aria-hidden="true">
+        {author.name.charAt(0).toUpperCase()}
+      </div>
+      <div className="bio-text">
+        <strong className="bio-name">{author.name}</strong>
+        {author.summary && (
+          <span className="bio-summary">{author.summary}</span>
+        )}
+      </div>
     </div>
   )
 }
